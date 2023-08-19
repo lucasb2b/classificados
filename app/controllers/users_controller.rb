2dd6b3ccf3883e2dfb9ed3751607ba8dcf79d148
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-18 22:41:37 -0300

    chore: create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to "/sessions/new", notice: "Cadastro realizado com sucesso!"
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
