cb0883abf6320f52a0ebf08579260fb15b926f91
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-18 22:10:55 -0300

    fix: Fixing the database

--- db/migrate/20230818022837_create_users.rb
+++ db/migrate/20230818022837_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
+  enable_extension "citext"
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email, null: false
+      t.citext :email, null: false
       t.string :password_digest, null: false
 
       t.timestamps
     end
     add_index :users, :email, unique: true
+    #add_index :users, "lower(email)", name: "idx_email_uniq", unique: true
   end
 end
