bb74c3546fc679e0dad39c1dab17cc609ab33713
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-21 22:42:15 -0300

    chore: flash messages and verification of login

--- test/system/logins_test.rb
+++ test/system/logins_test.rb
@@ -1,18 +1,34 @@
 require "application_system_test_case"
 
 class LoginsTest < ApplicationSystemTestCase
+  def setup
+    @user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
+  end
+
   test "Login com credenciais válidas" do
-    user = FactoryBot.create(:user, password: "password", password_confirmation: "password")
 
     visit root_path
     click_link "Entrar"
 
-    fill_in "E-mail", with: user.email
+    fill_in "E-mail", with: @user.email
     fill_in "Senha", with: "password"
 
     click_button "Entrar"
 
     assert_text "Bem vindo Darth Vader!"
+  end
+
+  test "login com credenciais invalidas" do
+    visit root_path
+    click_link "Entrar"
+
+    fill_in "E-mail", with: @user.email
+    fill_in "Senha", with: "invalidpassword"
+
+    click_button "Entrar"
+
+    assert_equal new_session_path, current_path
+    assert_text "E-mail ou senha inválidos"
   end
 
 end
