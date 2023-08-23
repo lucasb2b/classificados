14c786a5acfdf13485901a22d272a4e36062e557
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-22 21:03:44 -0300

    chore: Ads controller and methods

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,5 +1,20 @@
 class AdsController < ApplicationController
+  defore_action :require_logged_user
+
   def new
     @ad = Ad.new
+  end
+
+  def create
+    @ad = current_user.ads.build(ad_params)
+    if @ad.save
+      redirect_to root_path, notice: "Anúncio criado com sucesso!"
+    end
+  end
+
+  private
+
+  def ad_params
+    params.require(:ad).permit(:title, :description, :price)
   end
 end
